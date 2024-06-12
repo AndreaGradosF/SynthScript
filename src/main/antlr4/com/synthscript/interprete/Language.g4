314b87c7d9f61dad4b19f6ccc5039a5eeb7ad070
diego-calderonm <diego.calderonm8@gmail.com>
2024-06-11 22:36:55 -0500

    gramatica actualizada

--- src/main/antlr4/com/synthscript/interprete/Language.g4
+++ src/main/antlr4/com/synthscript/interprete/Language.g4
@@ -1,9 +1,47 @@
 grammar Language;
 
-start
-:
-	'hello' 'world'
-;
+programa
+    : declaracion*
+    ;
+
+declaracion
+    : declaracion_de_nota
+    | declaracion_de_pausa
+    | declaracion_de_secuencia
+    | declaracion_de_reproduccion
+    ;
+
+declaracion_de_nota
+    : NOTA ID IGUAL NOTA PARENTESIS_IZQ tono COMA duracion COMA octava PARENTESIS_DER PUNTO_Y_COMA
+    ;
+
+declaracion_de_pausa
+    : PAUSA ID IGUAL PAUSA PARENTESIS_IZQ duracion PARENTESIS_DER PUNTO_Y_COMA
+    ;
+
+declaracion_de_secuencia
+    : SECUENCIA ID IGUAL CORCHETE_IZQ elemento_secuencia* CORCHETE_DER PUNTO_Y_COMA
+    ;
+
+elemento_secuencia
+    : ID COMA?
+    ;
+
+declaracion_de_reproduccion
+    : REPRODUCIR PARENTESIS_IZQ ID PARENTESIS_DER PUNTO_Y_COMA
+    ;
+
+tono
+    : TONO
+    ;
+
+duracion
+    : DURACION
+    ;
+
+octava
+    : OCTAVA
+    ;
 
 NOTA: 'nota';
 PAUSA: 'pausa';
--- src/main/java/com/synthscript/interprete/Main.java
+++ src/main/java/com/synthscript/interprete/Main.java
@@ -1,30 +1,20 @@
 package com.synthscript.interprete;
 
-import java.io.IOException;
-import org.antlr.v4.runtime.CharStream;
+import com.synthscript.interprete.LanguageLexer;
+import com.synthscript.interprete.LanguageParser;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 
 public class Main {
-
-    private static final String EXTENSION = "lang";
-    private static final String DIRBASE = "src/test/resources/";
-
-    public static void main(String[] args) throws IOException {
-        String files[] = args.length==0? new String[]{ "test." + EXTENSION } : args;
-        System.out.println("Dirbase: " + DIRBASE);
-        for (String file : files){
-            System.out.println("START: " + file);
-
-            CharStream in = CharStreams.fromFileName(DIRBASE + file);
-            LanguageLexer lexer = new LanguageLexer(in);
-            CommonTokenStream tokens = new CommonTokenStream(lexer);
-            LanguageParser parser = new LanguageParser(tokens);
-            LanguageParser.StartContext tree = parser.start();
-            LanguageCustomVisitor visitor = new LanguageCustomVisitor();
-            visitor.visit(tree);
-
-            System.out.println("FINISH: " + file);
-        }
+    public static void main(String[] args) {
+        String input = "nota myNota = nota(A, 4, 3);";
+        
+        LanguageLexer lexer = new LanguageLexer(CharStreams.fromString(input));
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        LanguageParser parser = new LanguageParser(tokens);
+        
+        LanguageParser.ProgramaContext tree = parser.programa();
+        
+        System.out.println(tree.toStringTree(parser));
     }
 }
